105a61a00ed07df30a5abfdc38df21ef99f6412a
julie <7jujubuilds@gmail.com>
2024-12-11 22:55:42 +0900

    refactor(menu): 메뉴 확인 로직 리팩토링
    - 빠른 처리를 위해 메뉴 전체 목록을 상수화하여 문자열에서 메뉴 존재 여부를 확인하도록 수정

--- src/main/java/menu/domains/menu/Menu.java
+++ src/main/java/menu/domains/menu/Menu.java
@@ -7,23 +7,22 @@ import menu.ui.constants.ErrorMessages;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Menu {
     private final static List<String> categories = List.of("null", "일식", "한식", "중식", "아시안", "양식");
-    private final static Map<String, List<String>> allMenus = Map.of("일식", List.of("규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼".split(",")), "한식", List.of("김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음".split(",")), "중식", List.of("깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채".split(",")), "아시안", List.of("팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜".split(",")), "양식", List.of("라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니".split(",")));
+    private final static Map<String, String> allMenus = Map.of("일식", "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼", "한식", "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음", "중식", "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채", "아시안", "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜", "양식", "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니");
     private final List<String> finalCategories = new ArrayList<>();
 
     public static void isValidMenu(String name) {
-        allMenus.values().forEach(value -> {
-            if (!value.contains(name)) {
-                throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
-            }
-        });
+        String allMenusString = allMenus.values().toString();
+        if (!allMenusString.contains(name)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
+        }
     }
 
     public List<String> getMenuItems(String category) {
-        return allMenus.get(category);
+        String categoryMenus = allMenus.get(category);
+        return List.of(categoryMenus.split(", "));
     }
 
     public String getRandomCategory() {
@@ -35,7 +34,7 @@ public class Menu {
     }
 
     public void printCategories(OutputView outputView) {
-        String result = "[ 카테고리 " + String.join(" | ", this.finalCategories) + " ]";
+        String result = "[ 카테고리 | " + String.join(" | ", this.finalCategories) + " ]";
         outputView.printCategories(result);
     }
 }
