dd7fcad8058e801a6630bc9db5937ce3859c036e
julie <7jujubuilds@gmail.com>
2024-12-11 21:26:35 +0900

    feat(validator): 메뉴 검증 기능 구현

--- src/main/java/menu/helpers/Validator.java
+++ src/main/java/menu/helpers/Validator.java
@@ -1,11 +1,13 @@
 package menu.helpers;
 
+import menu.domains.menu.Menu;
 import menu.ui.constants.ErrorMessages;
 
 import java.util.List;
 
 public class Validator {
     private final static String DELIMITER = ",";
+    private final static List<String> categories = List.of("null", "일식", "한식", "중식", "아시안", "양식");
 
     public void checkNames(String input) {
         List<String> names = List.of(input.split(DELIMITER));
@@ -22,5 +24,19 @@ public class Validator {
                 throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
             }
         });
+    }
+
+    public void checkMenuNames(String input) {
+        Menu menu = new Menu();
+        List<String> names = List.of(input.split(DELIMITER));
+        int nameCount = names.size();
+        if (nameCount == 0) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
+        }
+        int uniqueCount = (int) names.stream().distinct().count();
+        if (nameCount != uniqueCount) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
+        }
+        names.forEach(menu::isValidMenu);
     }
 }
